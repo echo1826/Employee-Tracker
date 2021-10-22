8c2f21221697f7d7ab0d196c9681c24452a5d47d
Ethan C <ethan.cho1826@gmail.com>
2021-10-22 15:24:03 -0500

    connect db

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-const connection = require('./db/connection');
+const mysql = require('mysql2');
+const inquire = require('inquirer');
+// think about await and async for this initial inquirer
 
+const connection = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'password',
+        database: 'employees_db'
+    },
+    console.log(`Connected to employees_db database.`)
+);
