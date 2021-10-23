009cf8adde740c5b9fad97588691bd056cf352b8
Ethan C <ethan.cho1826@gmail.com>
2021-10-22 19:11:33 -0500

    promisify query and made class to hold queries, basic function logic done

--- assets/questions.js
+++ assets/questions.js
@@ -1,1 +1,35 @@
-// inquirer questions to import into server.js
+const Database = require('../db/queries');
+const connection = require('../db/connection');
+// inquirer questions to import into server.js for the add role, and add employee
+
+async function addEmployee() {
+    
+}
+
+async function addRole() {
+
+}
+
+async function addDepartment() {
+
+}
+
+async function viewEmployees() {
+    const db = new Database(connection);
+    const employees = await db.viewAllEmployees();
+    console.table(employees);
+}
+
+async function viewRoles() {
+    const db = new Database(connection);
+    const roles = await db.viewAllRoles();
+    console.table(roles);
+}
+
+async function viewDepartments() {
+    const db = new Database(connection);
+    const departments = await db.viewAllDepartments();
+    console.table(departments);
+}
+
+module.exports = {addEmployee, addRole, addDepartment, viewEmployees, viewRoles, viewDepartments};
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const mysql = require('mysql2');
 const inquire = require('inquirer');
+const {
+    addEmployee,
+    addRole,
+    addDepartment,
+    viewEmployees,
+    viewRoles,
+    viewDepartments
+} = require('./assets/questions');
+// BONUS update employee, view employee by manager, view employee by department, delete department, role, employee
+// view total salaries of all employees in department
 // think about await and async for this initial inquirer
 
 const connection = mysql.createConnection({
@@ -11,3 +21,67 @@ const connection = mysql.createConnection({
     },
     console.log(`Connected to employees_db database.`)
 );
+
+const answerHandler = async function (answer) {
+    switch (answer.choice) {
+        case "View all departments": {
+            // TODO: add function call here from queries.js
+            await askQuestion();
+            break;
+        }
+        case "View all roles": {
+            // TODO: add function call here from queries.js
+            await askQuestion();
+            break;
+        }
+        case "View all employees": {
+            // TODO: add function call here from queries.js
+            await askQuestion();
+            break;
+        }
+        case "Add a department": {
+            // TODO: add function call here from queries.js
+            await askQuestion();
+            break;
+        }
+        case "Add a role": {
+            // TODO: add function call here from queries.js
+            await askQuestion();
+            break;
+        }
+        case "Add an employee": {
+            // TODO: add function call here from queries.js
+            await askQuestion();
+            break;
+        }
+        case "Update an employee role": {
+            // TODO: add function call here from queries.js (bonus)
+            await askQuestion();
+            break;
+        }
+        default: {
+            return;
+        }
+    }
+}
+
+const askQuestion = async function () {
+    const choice = await inquire.prompt([{
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Quit"
+        ],
+        name: "choice"
+    }]).then((answer) => {
+        answerHandler(answer);
+    });
+}
+askQuestion();
